180c65b05b028cbd0fe807001e5276590fb9b6ab
Peiyu Wang <eraince@hotmail.com>
2018-08-14 21:56:23 -0400

    add the landing page.

---
+++ js/landing.js
@@ -0,0 +1,76 @@
+var logos = [];
+var facebookIcon;
+var likesIcon;
+var twitterIcon;
+var snapchatIcon;
+
+$(document).ready(function() {
+  $("#hello").click(function(e) {
+    e.preventDefault();
+    setTimeout(function() {
+      window.location.href = "index.html";
+    }, 1000);
+  });
+});
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  for (i = 0; i < 10; i++) {
+    logos[i] = new Logo(facebookIcon);
+  }
+  for (i = 10; i < 20; i++) {
+    logos[i] = new Logo(likesIcon);
+  }
+  for (i = 20; i < 30; i++) {
+    logos[i] = new Logo(twitterIcon);
+  }
+  for (i = 30; i < 40; i++) {
+    logos[i] = new Logo(snapchatIcon);
+  }
+}
+
+function preload() {
+  facebookIcon = loadImage("./img/facebook-2.png");
+  likesIcon = loadImage("./img/likes.png");
+  twitterIcon = loadImage("./img/twitter.png");
+  snapchatIcon = loadImage("./img/snapchat.png");
+}
+
+function draw() {
+  background(255);
+  for (i = 0; i < 40; i++) {
+    if (mouseIsPressed) {
+      logos[i].dead = true;
+    }
+    logos[i].move();
+    logos[i].display();
+  }
+}
+
+function Logo(icon) {
+  this.x = random(0, windowWidth);
+  this.y = random(0, windowHeight);
+  this.speedx = random(8, 15);
+  this.speedy = random(9, 20);
+  this.img = icon;
+  this.size = 50;
+  this.dead = false;
+
+  this.move = function() {
+    this.x += this.speedx;
+    this.y += this.speedy;
+    if (!this.dead) {
+      if (this.x > windowWidth || this.x < 0) {
+        this.speedx *= -1;
+      }
+      if (this.y > windowHeight || this.y < 0) {
+        this.speedy *= -1;
+      }
+    }
+  };
+
+  this.display = function() {
+    image(this.img, this.x, this.y, this.size, this.size);
+  };
+}
--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,10 @@
 var canvas = document.getElementById("draw");
-var color;
-mirror = document.getElementById("mirror");
+var pressed = false;
+var lineColor;
+var preX = 0;
+var preY = 0;
+var curX = 0;
+var curY = 0;
 
 // get canvas 2D context and set it to the correct size
 var ctx = canvas.getContext("2d");
@@ -12,53 +16,81 @@ function resize() {
   ctx.canvas.height = window.innerHeight;
 }
 
-// window.addEventListener("resize", resize);
-document.addEventListener("mousemove", draw);
-document.addEventListener("mousedown", setPosition);
-document.addEventListener("mouseenter", setPosition);
-document.addEventListener("resize", resize);
+canvas.addEventListener("mousemove", draw); // only draw when pressed is true
+canvas.addEventListener("mousedown", drawStart); // set pressed to true
+canvas.addEventListener("mouseup", drawStop); // set pressed to false
+canvas.addEventListener("mouseout", drawStop); // set pressed to false
+
 document.querySelector("input").addEventListener("input", function(e) {
   changeColor();
 });
 
 var pos = { x: 0, y: 0 };
 
-// new position from mouse events
-function setPosition(e) {
-  pos.x = e.offsetX;
-  pos.y = e.offsetY;
+// get current mouse position
+function getCurrentXPosition(event) {
+  return event.clientX - canvas.offsetLeft;
+}
+
+function getCurrentYPosition(event) {
+  return event.clientY - canvas.offsetTop;
+}
+
+function drawStop(event) {
+  pressed = false;
+}
+
+function drawStart(event) {
+  pressed = true;
+  setPosition(event);
+  lineColor = document.querySelector("input").value;
+  ctx.beginPath();
+  ctx.fillStyle = lineColor;
+  var circle = new Path2D();
+  circle.moveTo(curX, curY);
+  circle.arc(curX, curY, 10, 0, 2 * Math.PI);
+  ctx.fill(circle);
+  ctx.closePath();
+  console.log("start");
+}
+
+function draw(event) {
+  if (!pressed) return; // if not pressed.....
+
+  ctx.beginPath(); // begin the drawing path
+
+  ctx.lineWidth = 20; // width of line
+  ctx.lineCap = "round"; // rounded end cap
+  ctx.strokeStyle = lineColor; // hex color of line
+  console.log(lineColor);
+  setPosition(event);
+  ctx.moveTo(preX, preY); // from position
+  ctx.lineTo(curX, curY); // to position
+  ctx.stroke(); // draw it!
+  ctx.closePath();
+}
+
+function setPosition(event) {
+  preX = curX;
+  preY = curY;
+  curX = getCurrentXPosition(event);
+  console.log(curX);
+  curY = getCurrentYPosition(event);
+  console.log(curY);
 }
 
 function changeColor() {
-  color = document.querySelector("input").value;
+  lineColor = document.querySelector("input").value;
 }
 
-function saveCanvas() {
+function saveImage() {
   var dataURL = canvas.toDataURL("image/png");
   document.write('<img src="' + dataURL + '"/>');
 }
 
 function clearCanvas() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  faceSetup();
-}
-
-function draw(e) {
-  if (e.buttons !== 1) return; // if mouse is pressed.....
-
-  color = document.querySelector("input").value;
-
-  ctx.beginPath(); // begin the drawing path
-
-  ctx.lineWidth = 20; // width of line
-  ctx.lineCap = "round"; // rounded end cap
-  ctx.strokeStyle = color; // hex color of line
-
-  ctx.moveTo(pos.x, pos.y); // from position
-  setPosition(e);
-  ctx.lineTo(pos.x, pos.y); // to position
-
-  ctx.stroke(); // draw it!
+  // faceSetup();
 }
 
 function faceSetup() {
